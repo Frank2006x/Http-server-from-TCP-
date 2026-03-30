d30181a62d72c86c37eea9b566a8dc1655fca24b
Frank2006x <frank2006x@gmail.com>
2026-03-30 17:39:00 +0530

    tcp connection listener

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 )
 
 func getLinesChannel(f io.ReadCloser) <-chan string{
@@ -40,19 +40,24 @@ func getLinesChannel(f io.ReadCloser) <-chan string{
 	return out
 }
 
-
 func main() {
 	
-	f,err:=os.Open("message.txt")
+	listener,err:=net.Listen("tcp",":42069")
+
 	if(err!=nil){
-		log.Fatal("failed to open message.txt")
+		log.Fatal("err:",err)
 	}
-	
-	lines :=getLinesChannel(f)
-
-	for line:=range(lines){
-		fmt.Printf("Read : %s\n",line)
+	for {
+		conn,err:=listener.Accept()
+		if err!=nil{
+			log.Fatal("err:",err)
+		}
+		
+		lines :=getLinesChannel(conn)
+		
+		for line:=range(lines){
+			fmt.Printf("%s\n",line)
+		}
 	}
-
 
 }
